1848dba859d858a5d59b2e07f89d24155675ff7b
David <de203@uowmail.edu.au>
2016-11-26 12:14:10 +1100

    no message

---
+++ VN_Editor_KH16/VN_Editor_KH16/BackEnd/Flow_Elements/Generic_Element.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VN_Editor_KH16.BackEnd.Flow_Elements
+{
+    public abstract class Generic_Element
+    {
+    }
+}
---
+++ VN_Editor_KH16/VN_Editor_KH16/BackEnd/Flow_Elements/Slide_Element.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VN_Editor_KH16.BackEnd.Flow_Elements
+{
+    class Slide_Element : Generic_Element
+    {
+        public string speaker  { get; set; }
+        public string dialogue { get; set; }
+        public string dev_note { get; set; }
+    }
+}
