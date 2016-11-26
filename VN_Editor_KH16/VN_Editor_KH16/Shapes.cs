d27935a473410607fa93546ff8930eb1809eeefd
David <de203@uowmail.edu.au>
2016-11-26 19:21:45 +1100

    no message

---
+++ VN_Editor_KH16/VN_Editor_KH16/Shapes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+using System.Windows;
+
+namespace VN_Editor_KH16
+{
+    public class Shapes
+    {
+        public static Polygon loud_decision { get; set; } = new Polygon();
+        public static Polygon group { get; set; } = new Polygon();
+        public static Polygon slide { get; set; } = new Polygon();
+        public static Polygon end { get; set; } = new Polygon();
+        
+        Shapes ()
+        {
+            for (int i = 0; i < 40; i++)
+                end.Points.Add(new Point(10 * Math.Cos(2 * Math.PI * i / 40), 10 * Math.Sin(2 * Math.PI * i / 40)));
+
+            group.Points.Add(new Point(14, -10));
+            group.Points.Add(new Point(-14, -10));
+            group.Points.Add(new Point(-14, 10));
+            group.Points.Add(new Point(14, 10));
+
+            loud_decision.Points.Add(new Point(0, 14));
+            loud_decision.Points.Add(new Point(14, 0));
+            loud_decision.Points.Add(new Point(0, -14));
+            loud_decision.Points.Add(new Point(-14, 0));
+
+            slide.Points.Add(new Point(-10, 10));
+            slide.Points.Add(new Point(10, 10));
+            slide.Points.Add(new Point(10, -10));
+            slide.Points.Add(new Point(-10, -10));
+        }
+    }
+}
